9ded16bd03d3a27d4c14bddb9e9ffc332611b6e0
Daniel Fey <dan@crowdskout.com>
2017-04-08 03:57:13 -0400

    Adding ide helper

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,7 +23,7 @@ $app = new Laravel\Lumen\Application(
     realpath(__DIR__.'/../')
 );
 
-// $app->withFacades();
+$app->withFacades();
 
 // $app->withEloquent();
 
@@ -78,6 +78,7 @@ $app->singleton(
 |
 */
 
+$app->register(Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
