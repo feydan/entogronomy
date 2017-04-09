11aad694e4f955b0c03c07a64041846ef416f4e3
Daniel Fey <dan@crowdskout.com>
2017-04-08 22:09:43 -0400

    adding some autocomplete code

---
+++ public/js/entogronomy.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+	//$("form[name='search']").submit(function(e) {
+	//	e.preventDefault();
+	//	$("#results").html('');
+    //
+	//	var data = {
+	//		'insect_search': $("input[name='insect_search']")[0].value,
+	//		'plant_search': $("input[name='plant_search']")[0].value
+	//	};
+	//	console.log(data);
+	//
+	//	 $.ajax({
+	//		 'url': '/search',
+	//	 	 'type': 'POST',
+	//	 	 'data': data,
+	//		 'dataType': 'json',
+	//		 'success': function (search_responses) {
+	//			 var html_result = "";
+	//			 console.log(search_responses);
+	//			 $.each(search_responses, function (index, search_response) {
+	//				 html_result += "" +
+	//					 "<p>" +
+	//					 "<span>" + search_response.interaction.insect.common_name + "</span>" +
+	//					 "<span class='red'>" + search_response.interaction.verb + "</span>" +
+	//					 "<span>" + search_response.interaction.plant.common_name + "</span>" +
+	//					 "</p>";
+	//			 });
+    //
+	//			 $("#results").html(html_result);
+	//		 }
+	//	 });
+	//})
+	var insectData = function (request, response) {
+		var term = request.term.toLowerCase(); // convert search term to lowercase for case insensitive search
+		$.getJSON(
+			"api/insects",
+			function (insects) {
+				results = [];
+
+				// Loop through all of the insects
+				$.each(insects, function (index, insect) {
+
+					var commonName = insect.common_name.toLowerCase(); // convert insect common name to lowercase for case insensitive search
+					// If the common name starts with the term
+					if (commonName.startsWith(term)) {
+						results.push({
+							'label': insect.common_name,
+							'value': insect.insect_id
+						});
+					}
+				});
+
+				response(results);
+			});
+	};
+
+	$( "input[name='pest']" ).autocomplete({
+		appendTo: "#pest_menu",
+		source: insectData
+	});
+});
